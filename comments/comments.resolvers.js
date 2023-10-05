26eac68ef3b422a375af96fbec71666668efa4ec
hwadong119 <jeonghwayun0119@naver.com>
2023-10-05 13:48:49 +0900

    필터링 기능 추가

--- comments/comments.resolvers.js
+++ comments/comments.resolvers.js
@@ -4,6 +4,9 @@ module.exports = {
   Query: {
     comments: () => {
       return commentModel.getAllComments();
+    }, 
+    commentsByLikes: (_, args) => {
+      return commentModel.getCommentsByLikes(args.minLikes);
     }
   }
 }
